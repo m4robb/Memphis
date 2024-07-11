fd91ceafe561fbcbccccb746750754e1c46ca3ed
m4robb <m4robb@gmail.com>
2024-07-11 08:14:54 +0200

    General assets 3

---
+++ Assets/CustomPasses/Fur/Fur.cs.hlsl
@@ -0,0 +1,22 @@
+//
+// This file was automatically generated. Please don't edit by hand. Execute Editor command [ Edit > Rendering > Generate Shader Includes ] instead
+//
+
+#ifndef FUR_CS_HLSL
+#define FUR_CS_HLSL
+// Generated from Fur+FurData
+// PackingRules = Exact
+struct FurData
+{
+    float3 position;
+};
+
+//
+// Accessors for Fur+FurData
+//
+float3 GetPosition(FurData value)
+{
+    return value.position;
+}
+
+#endif
