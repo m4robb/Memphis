71898c03decea6275da3422d33759e1476490efd
m4robb <m4robb@gmail.com>
2024-07-11 08:08:59 +0200

    general assets 1

---
+++ Assets/blackburnecorp/InteractiveWaterShaderHDRP/Scripts/WaterDeformerScript/PlayerFootDeformerInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootDeformerInput : MonoBehaviour
+{
+    [SerializeField] private float force = 10.0f;
+    [SerializeField] private float forceOffset = 0.1f;
+
+    private Vector3 lastPos; 
+     void Start()
+    {
+        lastPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var displacement = transform.position - lastPos;
+        lastPos = transform.position; 
+         Ray _ray;
+        RaycastHit _raycastHit;
+        _ray = new Ray(this.transform.position, Vector3.down);
+ 
+        if (Physics.Raycast(_ray, out _raycastHit, 0.7f))
+        {
+             Debug.DrawLine(_ray.origin, _raycastHit.point,Color.magenta);
+            WaterDeformationManager deformer = _raycastHit.collider.GetComponent<WaterDeformationManager>();
+            if (deformer && displacement.magnitude >=0.02f)
+            {
+                Vector3 point = _raycastHit.point;
+                point += _raycastHit.normal * forceOffset;
+                deformer.ApplyDeformation(point, force);
+            }
+        }
+    }
+}
---
+++ Assets/blackburnecorp/InteractiveWaterShaderHDRP/Scripts/WaterDeformerScript/WaterDeformationManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+/// <summary>
+/// Main water deformation class
+/// Using Mesh API for deforming vertices and recalculating normals
+/// Attach to water object that has MeshFilter component 
+/// </summary>
+public class WaterDeformationManager : MonoBehaviour
+{
+    public float surfaceForce = 20f;
+    public float dampValue = 5f;
+    private  Mesh _deformingMesh;
+    private Vector3[] _initialVerticesArray;
+    private Vector3[] _deformedVerticesArray; 
+    private Vector3[] _verticesVelocitiesArray;
+    float uniformScale = 1f;
+
+    void Start()
+    {
+        _deformingMesh = GetComponent<MeshFilter>().mesh;
+        _initialVerticesArray = _deformingMesh.vertices;
+        _deformedVerticesArray = new Vector3[_initialVerticesArray.Length];
+        for (int i = 0; i < _initialVerticesArray.Length; i++)
+        {
+            _deformedVerticesArray[i] = _initialVerticesArray[i];
+        }
+        _verticesVelocitiesArray = new Vector3[_initialVerticesArray.Length];
+    }
+    void Update()
+    {
+        uniformScale = transform.localScale.x;
+        for (int i = 0; i < _deformedVerticesArray.Length; i++)
+        {
+            RefreshReformation(i);
+        }
+        _deformingMesh.vertices = _deformedVerticesArray;
+        _deformingMesh.RecalculateNormals();
+    }
+    /// <summary>
+    /// Refresh vertices positions and reform into normal shape after each deform attempt 
+    /// </summary>
+    /// <param name="i">vertex index</param>
+    void RefreshReformation(int index)
+    {
+        Vector3 velocity = _verticesVelocitiesArray[index];
+        Vector3 displacement = _deformedVerticesArray[index] - _initialVerticesArray[index];
+        displacement *= uniformScale;
+        velocity -= displacement * surfaceForce * Time.deltaTime;
+        velocity *= 1f - dampValue * Time.deltaTime;
+        _verticesVelocitiesArray[index] = velocity;
+        _deformedVerticesArray[index] += velocity * (Time.deltaTime / uniformScale);
+    }
+
+    /// <summary>
+    /// Apply deformation action from other scripts 
+    /// </summary>
+    /// <param name="point">Touch water position</param>
+    /// <param name="force">Deformation force</param>
+    public void ApplyDeformation(Vector3 touchPoint, float Appliedforce)
+    {
+        touchPoint = transform.InverseTransformPoint(touchPoint);
+        for (int i = 0; i < _deformedVerticesArray.Length; i++)
+        {
+            AddForceToVertex(i, touchPoint, Appliedforce);
+        }
+    }
+
+    /// <summary>
+    /// Apply deformation force on each vertex index from the mesh
+    /// </summary>
+    /// <param name="i">Vertex index</param>
+    /// <param name="point">Touch water position</param>
+    /// <param name="force">Deform force</param>
+    void AddForceToVertex(int index, Vector3 touchPoint, float force)
+    {
+        Vector3 pointToVertex = _deformedVerticesArray[index] - touchPoint;
+        pointToVertex *= uniformScale;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;
+        _verticesVelocitiesArray[index] += pointToVertex.normalized * velocity;
+    }
+}
